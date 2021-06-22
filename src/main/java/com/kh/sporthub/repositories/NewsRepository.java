ae11cafeceb38ea719021edcf319e273f6b76024
iamkh11 <iamkh11@outlook.com>
2021-06-22 16:14:49 +0100

    1

--- src/main/java/com/kh/sporthub/repositories/NewsRepository.java
+++ src/main/java/com/kh/sporthub/repositories/NewsRepository.java
@@ -1,6 +1,7 @@
 package com.kh.sporthub.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -17,7 +18,8 @@ public interface NewsRepository  extends MongoRepository<News, String>  {
 	public void delete(News news);
 	
 	    List<News> findAll();
-	
+	    
+	    Optional<News> findByid(String id) ;
 	
 	
 	
